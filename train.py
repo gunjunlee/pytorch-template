2c192e2a7a7d3dd48e77dfd62fd0181fa19e991d
kevin970401 <kevin970401@gmail.com>
2018-10-12 10:53:59 +0900

    add args

--- train.py
+++ train.py
@@ -8,6 +8,11 @@ def arg_parse():
     parser = argparse.ArgumentParser()
     add_arg = parser.add_argument
     add_arg('--data', default='./data/train', help='path to training data')
+    add_arg('--batch_size', default=32, type=int, help='batch size')
+    add_arg('--lr', default=1e-3, type=float, help='learning rate')
+    add_arg('--num_workers', default=8, type=int, help='num of workers')
+    add_arg('--gpu', default=True, help='using gpu')
+    
     args = parser.parse_args()
     return args
 
