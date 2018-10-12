647596e8286622d28e1bef04ba395a27f49ffd0a
kevin970401 <kevin970401@gmail.com>
2018-10-12 11:04:03 +0900

    add train.py

--- train.py
+++ train.py
@@ -18,3 +18,31 @@ def arg_parse():
 
 if __name__ == '__main__':
     args = arg_parse()
+    random.seed(1000)
+
+    '''suggested format
+    dataset = Dataset()
+    dataloader = Dataloader()
+    len_train = len(dataset) * 8 // 10
+    len_val = len(dataset) - len_train
+    train_dataloader, val_dataloader = dataloader.split([len_train, len_val])
+
+    net = Net()
+    if args.gpu:
+        net = net.cuda()
+        net = nn.DataParallel(net)
+    
+    criterion = Loss()
+    metric = Metric()
+    optimizer = optim.SGD(filter(lambda p:p.requires_grad, net.parameters()), 
+                          lr=args.lr, 
+                          momentum=0.9)
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+
+    model = Model(net)
+    model.compile(optimizer, criterion, metric, scheduler)
+    model.fit(train_dataloader=train_dataloader,
+              val_dataloader=val_dataloader,
+              epoch=args.epoch,
+              use_gpu=args.gpu)
+    '''
